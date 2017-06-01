8ef21096bb64b960d9328857ffed5e3af25f6dd9
Nick Eddy <nick.a.eddy@gmail.com>
2017-05-31 19:38:57 -0700

    made my own version of the clock project

---
+++ 02 - JS and CSS Clock/clockProject/main.js
@@ -0,0 +1,23 @@
+const secondHand = document.querySelector('.clock__hand--sec');
+const minuteHand = document.querySelector('.clock__hand--min');
+const hourHand = document.querySelector('.clock__hand--hour');
+
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    console.log(hours + ":" + minutes + ":" + seconds);
+
+}
+
+setInterval(setDate, 1000);
